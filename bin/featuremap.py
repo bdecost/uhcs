dcc8a87cfe3b2e54a3a98204c9b1bd07a904066f
Brian DeCost <bdecost@andrew.cmu.edu>
2016-11-15 12:18:15 -0500

    add controller script bin/featuremap.py. TODO: pull the dictionary extraction code out into perhaps the encoding module.

---
+++ bin/featuremap.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+""" controller script for uhcs project """
+import os
+import h5py
+import json
+import click
+import numpy as np
+from functools import partial
+from sklearn.externals import joblib
+from sklearn.cluster import MiniBatchKMeans
+
+# needed with slurm to see local python library under working dir
+import sys
+sys.path.append(os.getcwd())
+
+from mfeat import io
+from mfeat import cnn
+from mfeat import encode
+
+try:
+    from mfeat import local # import this downstream in case octave and/or oct2py are not available
+except OSError:
+    print('make sure octave, vlfeat, and oct2py are installed to use SIFT features')
+except ImportError:
+    print('make sure octave, vlfeat, and oct2py are installed to use SIFT features')
+
+layer_choices = cnn.layer_id.keys()
+
+def ensure_dir(path):
+    """ mkdir -p """
+    try: os.makedirs(path)
+    except: pass
+
+@click.command()
+@click.argument('micrographs_json', nargs=1, type=click.Path(),
+                help='specify image dataset (id: path)')
+@click.option('-k', '--n_clusters', help='number of dictionary words', default=100)
+@click.option('-s', '--style', default='ssift', help='sparse or dense sift; VGG',
+              type=click.Choice(['ssift', 'dsift', 'vgg16']), help='select image representation')
+@click.option('-e', '--encoding', default='bow', help='encoding method', type=click.Choice(['bow', 'vlad', 'fisher']),
+              help='select image feature pooling method')
+@click.option('-l', '--layername', default='block5_conv3', help='layer to extract', type=click.Choice(layer_choices),
+              help='select vgg16 convolution layer')
+def featuremap(micrographs_json, n_clusters, style, encoding, layername):
+    """ compute image representations for each image enumerated in micrographs_json 
+        results are stored in HDF5 keyed by the image ids in micrographs_json
+    """
+    dataset_dir, __ = os.path.split(micrographs_json)
+    ensure_dir(os.path.join(dataset_dir, 'dictionary'))
+    ensure_dir(os.path.join(dataset_dir, 'features'))
+                
+    if style in ['vgg16']:
+        method = '{}_{}'.format(style, layername)
+    else:
+        method = style
+        
+    metadata = {
+        'dir': dataset_dir,
+        'n_clusters': n_clusters,
+        'method': method,
+        'encoding': encoding
+    }
+    
+    # obtain a dataset
+    with open(micrographs_json, 'r') as f:
+        micrograph_dataset = json.load(f)
+
+    # work with sorted micrograph keys...
+    keys = sorted(micrograph_dataset.keys())
+    micrographs = [micrograph_dataset[key] for key in keys]
+    micrographs = [io.load_image(m) for m in micrographs]
+    
+    # set up paths
+    dictionary_file = '{dir}/dictionary/{method}-kmeans-{n_clusters}.pkl'.format(**metadata)
+    featurefile = '{dir}/features/{method}-{encoding}-{n_clusters}.h5'.format(**metadata)
+
+    if style == 'ssift':
+        extract_func = lambda mic, fraction=1.0: local.sparse_sift(mic, fraction=fraction)
+    elif style == 'dsift':
+        extract_func = lambda mic, fraction=1.0: local.dense_sift(mic, fraction=fraction)
+    elif style == 'vgg16':
+        extract_func = lambda mic, fraction=1.0: cnn.cnn_features(mic, layername, fraction=fraction)
+
+    try:
+        dictionary = joblib.load(dictionary_file)
+    except FileNotFoundError:
+        print('learning dictionary for {} images'.format(len(micrographs)))
+        training_fraction = 0.1
+
+        results = list(map(partial(extract_func, fraction=training_fraction), micrographs))
+        features = np.concatenate(results)
+
+        if encoding in ('bow', 'vlad'):
+            dictionary = MiniBatchKMeans(n_clusters=n_clusters, init='k-means++', n_init=25)
+        elif encoding in ('fisher'):
+            dictionary = GMM(n_components=n_clusters, covariance_type='diag')
+
+        print('clustering')
+        dictionary.fit(features)
+        print('done')
+        # serialize k-means results to disk
+        pkl_paths = joblib.dump(dictionary, dictionary_file)
+
+
+    encode_func = lambda mic: encode.vlad(extract_func(mic), dictionary)
+    features = map(encode_func, micrographs)
+    with h5py.File(featurefile, 'w') as f:
+        for key, feature in zip(keys, features):
+            f[key] = feature
+
+    
+    return
+
+if __name__ == '__main__':
+    featuremap()
